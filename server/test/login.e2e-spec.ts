0776c09d2008eb6338d77e6c4da296b3bf165538
JeanCarlosDelai <Jean_vieceli@outlook.com>
2024-03-28 15:25:38 -0300

    feat: :sparkles: Adicionados testes e2e da criação do Usuário

--- server/test/login.e2e-spec.ts
+++ server/test/login.e2e-spec.ts
@@ -9,7 +9,7 @@ let token = null;
 beforeAll(async () => {
   // Create User
   const createUserDto: CreateUserDto = {
-    name: 'teste',
+    name: 'teste222',
     email: 'teste.teste@teste.com',
     password: 'teste123',
   };
@@ -17,7 +17,6 @@ beforeAll(async () => {
   const responseCreate = await request('http://localhost:4000')
     .post('/users')
     .send(createUserDto);
-  console.log(`Response: ${responseCreate.body}`);
   userId = responseCreate.body.id;
 
   //Login
@@ -46,8 +45,8 @@ describe('/POST sessions', () => {
     const response = await request('http://localhost:4000')
       .post('/sessions')
       .send(loginData);
-    //Assert
     const body: UserAuthenticatedInterface = response.body;
+    //Assert
     expect(body.token).toBeDefined();
     expect(body.user).toBeDefined();
   });
@@ -67,5 +66,135 @@ describe('/POST sessions', () => {
     expect(response.status).toBe(HttpStatus401);
     expect(responseBody.error.error).toBe(Error);
     expect(responseBody.error.message).toBe(ErrorMessage);
+  });
+
+  it('Não deve ser possível efetuar com login com o email vazio', async () => {
+    //Arrange
+    const loginData = { email: '', password: 'teste123' };
+    const HttpStatus400 = 400;
+    const Error = 'Bad Request';
+    const ErrorMessage = 'O campo email não pode estar vazio';
+    //Act
+    const response = await request('http://localhost:4000')
+      .post('/sessions')
+      .send(loginData);
+    const responseBody = JSON.parse(response.text);
+    //Assert
+    expect(response.status).toBe(HttpStatus400);
+    expect(responseBody.error.error).toBe(Error);
+    expect(responseBody.error.message).toContain(ErrorMessage);
+  });
+
+  it('Não deve ser possível efetuar o login com a senha vazia', async () => {
+    //Arrange
+    const loginData = { email: 'teste.teste2@teste.com', password: '' };
+    const HttpStatus400 = 400;
+    const Error = 'Bad Request';
+    const ErrorMessage = 'O campo senha não pode estar vazio';
+    //Act
+    const response = await request('http://localhost:4000')
+      .post('/sessions')
+      .send(loginData);
+    const responseBody = JSON.parse(response.text);
+    //Assert
+    expect(response.status).toBe(HttpStatus400);
+    expect(responseBody.error.error).toBe(Error);
+    expect(responseBody.error.message).toContain(ErrorMessage);
+  });
+
+  it('Não deve ser possível efetuar o login com o email sendo um número', async () => {
+    //Arrange
+    const loginData = { email: 12345678, password: 'teste123' };
+    const HttpStatus400 = 400;
+    const Error = 'Bad Request';
+    const ErrorMessage = 'O campo email deve ser uma string';
+    //Act
+    const response = await request('http://localhost:4000')
+      .post('/sessions')
+      .send(loginData);
+    const responseBody = JSON.parse(response.text);
+    //Assert
+    expect(response.status).toBe(HttpStatus400);
+    expect(responseBody.error.error).toBe(Error);
+    expect(responseBody.error.message).toContain(ErrorMessage);
+  });
+
+  it('Não deve ser possível efetuar o login com a senha sendo um número', async () => {
+    //Arrange
+    const loginData = { email: 'teste.teste2@teste.com', password: 12345678 };
+    const HttpStatus400 = 400;
+    const Error = 'Bad Request';
+    const ErrorMessage = 'O campo senha deve ser uma string';
+    //Act
+    const response = await request('http://localhost:4000')
+      .post('/sessions')
+      .send(loginData);
+    const responseBody = JSON.parse(response.text);
+    //Assert
+    expect(response.status).toBe(HttpStatus400);
+    expect(responseBody.error.error).toBe(Error);
+    expect(responseBody.error.message).toContain(ErrorMessage);
+  });
+
+  it('Não deve ser possível efetuar o login com um email que não seja válido', async () => {
+    //Arrange
+    const loginData = { email: 'teste.teste', password: 'teste123' };
+    const HttpStatus400 = 400;
+    const Error = 'Bad Request';
+    const ErrorMessage = 'O campo email deve ser um email válido';
+    //Act
+    const response = await request('http://localhost:4000')
+      .post('/sessions')
+      .send(loginData);
+    const responseBody = JSON.parse(response.text);
+    //Assert
+    expect(response.status).toBe(HttpStatus400);
+    expect(responseBody.error.error).toBe(Error);
+    expect(responseBody.error.message).toContain(ErrorMessage);
+  });
+
+  it('Não deve ser possível efetuar o login com um email ou senha com menos caracteres que o mínimo', async () => {
+    //Arrange
+    const loginData = { email: '1@1.c', password: '123' };
+    const HttpStatus400 = 400;
+    const Error = 'Bad Request';
+    const ErrorMessageEmail =
+      'O campo email não pode ter menos que 6 caracteres';
+    const ErrorMessagepassword =
+      'O campo senha não pode ter menos que 6 caracteres';
+    //Act
+    const response = await request('http://localhost:4000')
+      .post('/sessions')
+      .send(loginData);
+    const responseBody = JSON.parse(response.text);
+    //Assert
+    expect(response.status).toBe(HttpStatus400);
+    expect(responseBody.error.error).toBe(Error);
+    expect(responseBody.error.message).toContain(ErrorMessageEmail);
+    expect(responseBody.error.message).toContain(ErrorMessagepassword);
+  });
+
+  it('Não deve ser possível efetuar o login com um email ou senha com mais caracteres que o máximo', async () => {
+    //Arrange
+    const loginData = {
+      email: '1234567890123456789012345678901234567890@gmail.com.br',
+      password: '1234567890123465798901',
+    };
+    const HttpStatus400 = 400;
+    const Error = 'Bad Request';
+    const ErrorMessageEmail =
+      'O campo email não pode ter mais que 40 caracteres';
+    const ErrorMessagepassword =
+      'O campo senha não pode ter mais que 20 caracteres';
+    //Act
+    const response = await request('http://localhost:4000')
+      .post('/sessions')
+      .send(loginData);
+    const responseBody = JSON.parse(response.text);
+    //Assert
+    expect(response.status).toBe(HttpStatus400);
+    expect(responseBody.error.error).toBe(Error);
+    expect(responseBody.error.message).toContain(ErrorMessageEmail);
+    expect(responseBody.error.message).toContain(ErrorMessagepassword);
   });
 });
